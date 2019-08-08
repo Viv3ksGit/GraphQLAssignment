0f1d85a9669e1428c67a6fb6994d58366b94baef
Vivek <Viv3kdev@gmail.com>
2019-08-08 18:01:22 -0400

    Fetch’s “title” & “director” of “allFilms”from the given GraphQL API and Display data from that API in List View

--- GraphQLAssignment/MainPage.xaml.cs
+++ GraphQLAssignment/MainPage.xaml.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
 using Xamarin.Forms;
+using GraphQLAssignment.Model;
 
 namespace GraphQLAssignment
 {
@@ -13,9 +18,27 @@ namespace GraphQLAssignment
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            var client = new GraphQLClient("https://swapi.apis.guru/");
+
+            GraphQLRequest graphQLRequest = new GraphQLRequest
+            {
+                Query = "query{ allFilms { films {title, director,id, releaseDate } } }"
+            };
+            GraphQLResponse graphQLResponse = await client.PostAsync(graphQLRequest);
+            starwarsList.ItemsSource = graphQLResponse.Data.allFilms.films.ToObject<List<StarwarsApi>>();
+
+        }
+
     }
 }
--- GraphQLAssignment/Model/StarwarsApi.cs
+++ GraphQLAssignment/Model/StarwarsApi.cs
@@ -1,20 +1,36 @@
 ﻿using System;
-
-using Xamarin.Forms;
-
 namespace GraphQLAssignment.Model
 {
-    public class StarwarsApi : ContentPage
-    {
-        public StarwarsApi()
-        {
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Hello ContentPage" }
-                }
-            };
-        }
-    }
-}
 
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class StarwarsApi
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string id { get; set; }
+        public string releaseDate { get; set; }
+
+        public string directorAndreleaseDate { get { return "Director  " +director + "|| Release Date  " + releaseDate + "||ID-" + id; } }
+
+    }
+
+
+
+    //public partial class Data
+    //{
+    //    public allFilms allFilms { get; set; }
+    //}
+
+    //public partial class allFilms
+    //{
+    //    public List<Film> Films { get; set; }
+    //}
+
+    //public partial class Film
+    //{
+
+    //}
+}
