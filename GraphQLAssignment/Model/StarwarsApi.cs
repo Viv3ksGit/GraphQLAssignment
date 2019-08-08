5745761294ce79e7d8075bda90296c1c4778543a
Vivek <Viv3kdev@gmail.com>
2019-08-08 17:58:09 -0400

    First commit

---
+++ GraphQLAssignment/Model/StarwarsApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace GraphQLAssignment.Model
+{
+    public class StarwarsApi : ContentPage
+    {
+        public StarwarsApi()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
